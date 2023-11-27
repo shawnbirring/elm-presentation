885f0b4190988dbc48e1249e809a5da7d20f477b
shawnbirring <shawnbirring21@gmail.com>
2023-11-26 22:14:22 -0800

    immutability

---
+++ src/Immutability.elm
@@ -0,0 +1,13 @@
+module Immutability exposing (..)
+
+import Html exposing (Html, text)
+
+add : Int -> Int -> Int
+add a b =
+    let sum = a + b
+        sum = sum + 1
+    in sum
+
+main : Html msg
+main =
+    text (String.fromInt (add 1 2))
